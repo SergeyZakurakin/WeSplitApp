ab716f263533a71a3d2701bcc093ddeffe1c185a
SergeyZakurakin <pricezak@gmail.com>
2024-08-30 21:27:26 -0400

    create project

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sergey Zakurakin on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
